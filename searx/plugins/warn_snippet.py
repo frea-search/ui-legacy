2a02442194fb1a85d9fbde8a7e000d1ef279ea65
nexryai <nexryai@outlook.com>
2022-07-02 11:40:15 +0900

    プラグイン追加

---
+++ searx/plugins/warn_snippet.py
@@ -0,0 +1,11 @@
+name = "Warning snippet"
+description = gettext("Displays advice and warnings.")
+default_on = True
+
+def post_search(request, search):
+    if search.search_query.pageno > 1:
+        return True
+    if 'コロナ' in search.search_query.query:
+        message = "新型コロナウイルス感染症や新型コロナウイルスワクチンに関する正しい情報をお求めの場合は、WHOのページをご確認ください。"
+    search.result_container.answers['warning'] = {'answer': message}
+    return True
