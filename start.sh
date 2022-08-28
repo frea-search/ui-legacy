108cee7fbd125f7cf687766031a6e8d726e73219
nexryai <nexryai@outlook.com>
2022-08-29 08:46:15 +0900

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/etc/searxng/settings.yml"
+f grep -q ultrasecretkey /etc/searxng/settings.yml; then
+    sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/etc/searxng/settings.yml"
+fi
+
 uwsgi --master --http-socket 8888 /usr/local/searxng/dockerfiles/uwsgi.ini
