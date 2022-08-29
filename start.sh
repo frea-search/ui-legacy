4f4611971a7c2e6ce28ff45a5e901ae90f607206
nexryai <nexryai@outlook.com>
2022-08-29 09:15:53 +0900

    fix typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-f grep -q ultrasecretkey /etc/searxng/settings.yml; then
+if grep -q ultrasecretkey /etc/searxng/settings.yml; then
     sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/etc/searxng/settings.yml"
 fi
 
