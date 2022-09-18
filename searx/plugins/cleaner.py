ef8578711269e1359c4adaba9ce8262c2af2ff06
nexryai <nexryai@outlook.com>
2022-09-19 06:59:17 +0900

    Add cleaner plugin

---
+++ searx/plugins/cleaner.py
@@ -0,0 +1,30 @@
+name = "Result Cleaner"
+description = "Clean results."
+default_on = True
+
+def is_included_in_list(title, words_list):
+    for detect_word in words_list:
+        if detect_word in title:
+            return True
+        return False
+
+def on_result(request, search, result):
+
+    result_title=result['title']
+
+    detect_words_1=['コロナ', 'ワクチン', 'マスク']
+    detect_words_2=['強制', '強要', '陰謀']
+    block_words=['現役エンジニア', '徹底解説', '枠珍']
+
+    if is_included_in_list(result_title, detect_words_1):
+        if is_included_in_list(result_title, detect_words_2):
+            return False
+        else:
+            return True
+
+    elif is_included_in_list(result_title, block_words):
+        return False
+
+    else:
+        return True
+
