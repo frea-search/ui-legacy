422e254ff49f04283483679ccab46b5943de635b
nexryai <nexryai@outlook.com>
2022-10-04 17:08:23 +0900

    Add intelligence_engine

---
+++ searx/plugins/intelligence_engine.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import requests
+import json
+
+name = "Intelligence Engine"
+description = "Displays useful informations."
+default_on = True
+
+
+def post_search(request, search):
+    if search.search_query.pageno > 1:
+        return True
+    if '天気' in search.search_query.query:
+        #ToDo
+    return True
