42fa293b0e6714f04d2081ba638f7618fc3285bd
nexryai <nexryai@outlook.com>
2022-10-16 21:58:45 +0900

    Enhanced security for secrets

--- dockerfiles/init-server.sh
+++ dockerfiles/init-server.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+export SEARXNG_SETTINGS_PATH=/etc/frea/settings.yml
+export SEARXNG_SECRET=$(openssl rand -hex 16)
+
 cp /var/frea/dockerfiles/uwsgi.ini /etc/searxng/uwsgi.ini
 
 if [ -e /etc/searxng/settings.yml ]; then
@@ -9,8 +12,8 @@ if [ ! -e /etc/frea/settings.yml ]; then
   cp /var/frea/utils/templates/etc/frea/settings.yml /etc/frea/settings.yml
 fi
 
-if grep -q ultrasecretkey /etc/frea/settings.yml; then
-  sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/etc/frea/settings.yml"
+if grep -q ultrasecretkey /var/frea/searx/settings.yml; then
+  sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/var/frea/searx/settings.yml"
 fi
 
 su frea -c "uwsgi --master --http-socket 0.0.0.0:8888 /etc/searxng/uwsgi.ini"
