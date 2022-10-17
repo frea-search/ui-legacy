1b80455d311324d91fe730015892edc12db3635c
nexryai <nexryai@outlook.com>
2022-10-17 21:53:13 +0900

    Update warn_snippet

--- searx/plugins/warn_snippet.py
+++ searx/plugins/warn_snippet.py
@@ -8,16 +8,23 @@ def post_search(request, search):
     if search.search_query.pageno > 1:
         return True
     if 'コロナ' in search.search_query.query:
-        message = "新型コロナウイルス感染症に関する正しい情報をお求めの場合は、厚生労働省のwebサイト(https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html)をご確認ください。"
+        message = "新型コロナウイルス感染症に関する正しい情報をお求めの場合は、厚生労働省のwebサイトをご確認ください。"
+        link = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html"
     if 'ワクチン' in search.search_query.query:
-        message = "新型コロナワクチンに関する信頼できる情報をお求めの場合は、公的機関のページ(https://v-sys.mhlw.go.jp/)が役に立つでしょう。"
+        message = "新型コロナワクチンに関する信頼できる情報をお求めの場合は、公的機関のページが役に立つでしょう。"
+        link = "https://v-sys.mhlw.go.jp/"
     if 'サル痘' in search.search_query.query:
-        message = "サル痘に関する情報をお求めの場合は、厚生労働省のwebサイト(https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/kekkaku-kansenshou19/monkeypox_00001.html)をご確認ください。"
+        message = "サル痘に関する情報をお求めの場合は、厚生労働省のwebサイトをご確認ください。"
+        link = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou/kekkaku-kansenshou19/monkeypox_00001.html"
     if 'ウクライナ' in search.search_query.query:
-        message = "ユニセフの緊急募金(https://www.unicef.or.jp/kinkyu/ukraine/)に参加しウクライナを支援できます。"
+        message = "ユニセフの緊急募金に参加しウクライナを支援できます。"
+        link = "https://www.unicef.or.jp/kinkyu/ukraine/"
     
     if 'message' in locals():
-        search.result_container.answers['warning'] = {'answer': message}
+        if 'link' in locals():
+            search.result_container.answers['warning'] = {'answer': message, 'url': link}
+        else:
+            search.result_container.answers['warning'] = {'answer': message}
     
     return True
         
