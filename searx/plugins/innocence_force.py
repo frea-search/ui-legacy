5fbccab50f36a3c32e361b41356c571120f4b55e
nexryai <nexryai@outlook.com>
2022-10-19 20:22:48 +0900

    Update searx/plugins/innocence_force.py

--- searx/plugins/innocence_force.py
+++ searx/plugins/innocence_force.py
@@ -1,9 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import requests
+
 name = "Innocence force"
 description = "Displays useful informations."
 default_on = True
 
+parsed = "parsed_url"
 
-def on_result(request, search, result):
-    return True
+#def on_result(request, search, result):
+#   if parsed in result:
+#        pass
+#    return True
