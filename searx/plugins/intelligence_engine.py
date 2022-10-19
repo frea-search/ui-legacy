64d74292267d37fd19a66873ec4c04f43801febe
nexryai <nexryai@outlook.com>
2022-10-19 20:22:26 +0900

    Add tsunami warning

--- searx/plugins/intelligence_engine.py
+++ searx/plugins/intelligence_engine.py
@@ -24,11 +24,20 @@ class Client:
     
 
 def post_search(request, search):
+    tsunami_client = Client('/var/frea/tmp/org.freasearch.intelligence-engine/tsunami.sock')
+    tsunami_info = tsunami_client.request(search.search_query.query)
+    tsunami = ast.literal_eval(tsunami_info)
+
+    if tsunami["no_tsunami"] != "true":
+        message = "警告: 現在、一部の沿岸地域に" + tsunami["grade"] + "が発表されています。"
+        search.result_container.answers['tsunami'] = {'message': message, 'tsunami': 'true', 'hide_icon': 'true'}
+        return True
+
     if search.search_query.pageno > 1:
         return True
     if '天気' in search.search_query.query:
-        client = Client('/var/frea/tmp/org.freasearch.intelligence-engine/weather.sock')
-        api_result = client.request(search.search_query.query)
+        weather_client = Client('/var/frea/tmp/org.freasearch.intelligence-engine/weather.sock')
+        api_result = weather_client.request(search.search_query.query)
         result = ast.literal_eval(api_result)
         #print(chardet.detect(result))
         message="現在の天気: " + result['weather']
