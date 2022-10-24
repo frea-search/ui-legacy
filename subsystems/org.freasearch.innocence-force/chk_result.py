79dbd3a68b132e7feea0637ea679c767bdeeb638
nexryai <nexryai@outlook.com>
2022-10-24 15:49:30 +0900

    Improved handling of subdomains

--- subsystems/org.freasearch.innocence-force/chk_result.py
+++ subsystems/org.freasearch.innocence-force/chk_result.py
@@ -10,11 +10,12 @@ import tldextract
 
 def chk_result(result):
     url = result["url"]
+    domain = result["parsed_url"][1]
     title = result["title"]
     extracted = tldextract.extract(url)
-    domain = "{}.{}".format(extracted.domain, extracted.suffix)
+    root_domain = "{}.{}".format(extracted.domain, extracted.suffix)
     
-    if blocklist.chk_domain(domain):
+    if blocklist.chk_domain(root_domain, domain):
         return False
     elif blockwords.chk_title(title):
         return False
