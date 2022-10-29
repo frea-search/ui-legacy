af1f8141fcd23de8bdcdfd5ea6caa8302e819e55
nexryai <nexryai@outlook.com>
2022-10-29 16:32:32 +0900

    Uodate innocence-force

--- subsystems/org.freasearch.innocence-force/blockwords.py
+++ subsystems/org.freasearch.innocence-force/blockwords.py
@@ -3,6 +3,22 @@ import os
 import yaml
 import msg
 
+def is_in_untrusted_domain(root_domain, domain):
+    try:
+        with open(f"/var/frea/subsystems/org.freasearch.innocence-force/blockwords/untrusted_domains.yml", "r", encoding="utf8") as yml:
+            untrusted_domains = yaml.safe_load(yml)
+    except Exception as e:
+        msg.error("[ERROR] faild to load blockwords")
+        msg.fetal_error(e)
+        return False
+    else:
+        if root_domain in untrusted_domains["domains"]:
+            return True
+        elif domain in untrusted_domains["domains"]:
+            return True
+        else:
+            return False
+
 
 def is_in_blockwords(title, listname):
     try:
@@ -19,13 +35,17 @@ def is_in_blockwords(title, listname):
         return False
             
 
-def chk_title(title):
-    if is_in_blockwords(title, "detect_words_1"):
-        if is_in_blockwords(title, "detect_words_2"):
+def chk_title(title, content, root_domain, domain):
+    if is_in_blockwords(title, "detect_words"):
+        if is_in_untrusted_domain(root_domain, domain):
             return True
-        else:
-            return False
-    elif is_in_blockwords(title, "block_words"):
+
+    if connect != "NO_DATA":
+        if is_in_blockwords(content, "detect_words"):
+            if is_in_untrusted_domain(root_domain, domain):
+                return True
+
+    if is_in_blockwords(title, "block_words"):
         return True
-    else:
-        return False
+
+    return False
