e60a5052b511f9653a64c6bac092e3ffceeb3797
nexryai <gnomer@tuta.io>
2022-11-18 22:02:45 +0900

    Remove files

--- subsystems/org.freasearch.innocence-force/get_blocklist.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Upstream lists
-# https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
-# https://hosts.ubuntu101.co.za/hosts
-# https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt
-# https://malware-filter.gitlab.io/malware-filter/pup-filter-hosts.txt
-# https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-hosts.txt
--- subsystems/org.freasearch.intelligence-engine/init/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm -f /var/frea/tmp/org.freasearch.intelligence-engine/train.sock
-su frea -c "python3 /var/frea/subsystems/org.freasearch.intelligence-engine/train.py"
